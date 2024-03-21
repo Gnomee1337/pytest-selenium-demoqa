7dc20db5c7405c047e5ccb5af8762abd04894c2f
gnomee1337 <41423212+Gnomee1337@users.noreply.github.com>
2024-03-21 18:37:57 +0200

    base test-box check-box

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope='function')
+def driver():
+    adblock_path = r"./AdGuard-AdBlocker.crx"
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_extension(adblock_path)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
---
+++ tests/test_elements.py
@@ -0,0 +1,23 @@
+import time
+
+from pages.elements_page import TextBoxPage, CheckBoxPage
+
+
+class TestElements:
+    class TestTextBox:
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            input_data = text_box_page.fill_all_fields()
+            output_data = text_box_page.check_filled_form()
+            assert input_data == output_data, "input_data doesnt match output_data in Text Box"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_data = check_box_page.get_checked_checkboxes()
+            output_data = check_box_page.get_output_result()
+            assert input_data == output_data, 'selected checkboxes doesnt match in Check Box'
